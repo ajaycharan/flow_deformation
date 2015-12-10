dda8b2d200ad31a0e94b32410ec8e62f8bf37c88
Armon <armon.shariati@gmail.com>
2015-12-10 16:13:49 -0500

    Moved a function

--- simlogm.m
+++ simlogm.m
@@ -70,3 +70,29 @@ B(1:3, end) = W;
 
 end
 
+function U = compute_ssm(S)
+
+% vector of b, c, d
+x = zeros(3, 1);
+
+% vectorize diagonal and add 1
+d = diag(S) + 1;
+
+% find first nonzero root
+id = find(d, 1);
+r = sqrt(d(id));
+
+% compute final b, c, d
+x(id) = r;
+y = S(id, :)';
+y(id) = 0;
+x = x + y / r;
+
+% generate matrix
+b = x(1);
+c = x(2);
+d = x(3);
+
+U = [0, -d, c; d, 0, -b; -c, b, 0];
+
+end
